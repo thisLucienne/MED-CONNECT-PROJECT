27736dba6e53ff50b5ce281e1beb5445c627accb
Lucienne monac <luciennekoubissack@gmail.com>
2025-11-21 19:21:34 +0100

    feat(user-service): impémentation des vues des médécins

--- Frontend/med-connect-web/src/app/app.routes.ts
+++ Frontend/med-connect-web/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login.components/login.components';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PatientDComponent } from './components/patient_d/patient_d.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'patients', component: PatientDComponent },
+  { path: '**', redirectTo: '/login' }
+];
